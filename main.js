e3094bda16d017b2328ee83cf03f04d536427f46
pang.frontend <pang.frontend@gmail.com>
2023-12-28 20:42:06 +0700

    done

---
+++ main.js
@@ -0,0 +1,7 @@
+let burgerMenu = document.getElementById('burger-menu');
+let overlay = document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function () {
+    this.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+});
